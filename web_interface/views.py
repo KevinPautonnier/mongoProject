9a2624e3fe13ea1e36559d7630eaae12d61cb949
antoine <antoine.gosset@ynov.com>
2017-05-03 15:18:32 +0200

    client style added and data binding

--- web_interface/views.py
+++ web_interface/views.py
@@ -2,15 +2,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from pymongo import MongoClient
+from datetime import date
 from django.template.response import TemplateResponse
 import pprint
+import datetime
+import pytz
+
 
 databaseName = "BooBook"
 
-client = MongoClient('192.168.182.128', 27017)
+client = MongoClient('192.168.117.134', 27017)
 
 db = client[databaseName]
-
+clientsliste = db['clients']
+collection = db['books']
 
 def home(request):
     return TemplateResponse(request, 'index.html')
@@ -21,14 +26,42 @@ def orders(request):  # Rendering a speficic order
 
 
 def clients(request):  # Rendering a speficic order
-    return TemplateResponse(request, 'clients.html')
+
+    clientrequest = clientsliste.find()
+    return TemplateResponse(request, 'clients.html', {"clients": clientrequest})
 
 
 def books(request):
-    collection = db['books']
+
     test = collection.find()
     return TemplateResponse(request, 'books.html', {"books": test})
 
 
 def stats(request):  # Rendering a speficic order
-    return TemplateResponse(request, 'stats.html')
+
+    year20 = datetime.timedelta(days = -(20 *365))
+    year40 = datetime.timedelta(days = -(40*365))
+
+    vingt = (datetime.datetime.now(pytz.utc) + year20).isoformat()
+
+    quarante = (datetime.datetime.now(pytz.utc) + year40).isoformat()
+
+    print "20"
+    print vingt
+    print "40"
+    print quarante
+
+    client20to40 = clientsliste.find(
+        {'birth_date':{'$gt': (vingt), '$lt': (quarante)}}
+    )
+
+
+
+    # Femme la plus agée
+
+    oldestwomen = clientsliste.find( {'gender' : 'Female', }).sort({ "birth_date" : 1 }).limit(1)
+
+
+
+
+    return TemplateResponse(request, 'stats.html', {"clients": client20to40}, {"oldest" : oldestwomen})
