135996fd24f29124bde3bd8471387a88d08c98bb
antoine <antoine.gosset@ynov.com>
2017-05-17 16:35:22 +0200

    Plus vieille femme et adresse email ajouté en stats

--- web_interface/views.py
+++ web_interface/views.py
@@ -7,10 +7,10 @@ from django.template.response import TemplateResponse
 import pprint
 import datetime
 import pytz
-
+import pymongo
 databaseName = "BooBook"
 
-client = MongoClient('192.168.117.134', 27017)
+client = MongoClient('192.168.152.133', 27017)
 
 db = client[databaseName]
 clientsliste = db['clients']
@@ -19,22 +19,18 @@ collection = db['books']
 def home(request):
     return TemplateResponse(request, 'index.html')
 
-
 def orders(request):  # Rendering a speficic order
     return TemplateResponse(request, 'orders.html')
-
 
 def clients(request):  # Rendering a speficic order
 
     clientrequest = clientsliste.find()
     return TemplateResponse(request, 'clients.html', {"clients": clientrequest})
 
-
 def books(request):
 
     test = collection.find()
     return TemplateResponse(request, 'books.html', {"books": test})
-
 
 def stats(request):  # Rendering a speficic order
 
@@ -44,34 +40,21 @@ def stats(request):  # Rendering a speficic order
     vingt = (datetime.datetime.now(pytz.utc) + year20).isoformat()
     quarante = (datetime.datetime.now(pytz.utc) + year40).isoformat()
 
-    print "20"
-    print vingt
-    print "40"
-    print quarante
-
     client20to40 = clientsliste.find(
-        {'birth_date':{'$gt': (quarante), '$lt': (vingt)}}
+        {'gender' : 'Male', 'birth_date':{'$gt': (quarante), '$lt': (vingt)}}
     )
-
-    """for i in client20to40:
-        print i["birth_date"]
-
-    #Nb adrese mail avec chiffres
-
-    emailnumber = clientsliste.find()
-    nbadresse = 0
-    for email in emailnumber :
-        #print email["email"]
-
-        if "0" in list(str(email["email"]))or "1" in list(str(email["email"]))or "2" in list(str(email["email"]))or "3" in list(str(email["email"]))or "4" in list(str(email["email"]))or"5" in list(str(email["email"]))or "6" in list(str(email["email"]))or "7" in list(str(email["email"]))or "8" in list(str(email["email"]))or "9" in list(str(email["email"])):
-            nbadresse += 1
-
-    print nbadresse
 
     #Femme la plus agée
 
-    # oldestwomen = clientsliste.find( {'gender' : 'Female', }).sort({ "birth_date" : 1 }).limit(1)"""
+    oldestwomen = clientsliste.find({'gender': 'Female'}).sort('birth_date', pymongo.ASCENDING).limit(1)
 
-    return TemplateResponse(request, 'stats.html', {"stats": client20to40})
-
-
+    mailnumber = clientsliste.find()
+    test = 0
+    print type(mailnumber)
+    for mail in mailnumber:
+        if '0' in str(mail["email"]) or '1' in str(mail["email"]) or '2' in str(mail["email"]) or '3' in str(mail["email"]) or '4' in str(mail["email"]) or '5' in str(mail["email"]) or '6' in str(mail["email"]) or '7' in str(mail["email"]) or '8' in str(mail["email"]) or '9' in str(mail["email"]):
+            test = test + 1
+        else:
+            print mail["email"]
+    print test
+    return TemplateResponse(request, 'stats.html', {"femmes": oldestwomen, "stats": client20to40, "mails": mailnumber})
